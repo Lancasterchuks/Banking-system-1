d46bbe2184c21bc8a236ecac7180feb13370bd5a
Lancasterchuks <122053912+Lancasterchuks@users.noreply.github.com>
2023-12-28 11:31:57 +0000

    updated code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class BankSystem():
-  def __iclass BankSyclass BankSystem:
+  
     def __init__(self, balance):
         self.balance = balance
 
@@ -15,75 +15,20 @@ if user_input.lower() == "yes":
     bank.deposit()
 else:
     print("No deposit will be made.")
- stem():
-def __init__(self, balance): 
-      self.balance = balance
+  
+  
+    def withdraw(self)
+        confirm_withdraw = input("Are you sure you want to withdraw? (yes/no)")
+        if confirm_withdraw.lower() == "yes":
+            withdraw_amount = float(input("How much would you like to withdraw?"))
+            if withdraw_amount > self.balance:
+                print("Insufficient funds")
+            else:
+                self.balance -= withdraw_amount
+                print("Withdrawal successful")
 
-  def deposit(self):
-      deposit_amount = float(input("How much would you like to deposit?"))
-      # Add the deposit_amount to the account balance
-      self.balance += deposit_amount
-      print("Deposit successful")
-
-bank = BankSystem(100)  # Example: initial balance is 100
-
-user_input = input("Do you want to deposit? (yes/no)")
-if user_input.lower() == "yes":
-    bank.deposit()
-else:
-    print("No deposit will be made.")
-nit__(self,balance): 
-      self.balance = balance
-
-  def deposit(self):class BankSystem():
-  def __init__(self, balance): 
-      self.balance = balance
-
-  def deposit(self):
-      deposit_amount = float(input("How much would you like to deposit?"))
-      # Add the deposit_amount to the account balance
-      self.balance += deposit_amount
-      print("Deposit successful")
-
-bank = BankSystem(100)  # Example: initial balance is 100
-user_input = input("Do you want to deposit? (yes/no)")
-if user_input.lower() == "yes":
-    bank.deposit()
-else:
-    print("No deposit will be made.")
-
-   confirm_class BankSystem():
-  def __init__(self, balance): 
-      self.balance = balance
-
-  def deposit(self):
-      deposit_amount = float(input("How much would you like to deposit?"))
-      # Add the deposit_amount to the account balance
-      self.balance += deposit_amount
-      print("Deposit successful")
-
-bank = BankSystem(100)  # Example: initial balance is 100
-user_input = input("Do you want to deposit? (yes/no)")
-if user_input.lower() == "yes":
-    bank.deposit()
-else:
-    print("No deposit will be made.")
-
-confirm_deposit = input("How much would you like to deposit")
-user_input = input("Do you want to deposit? (yes/no)")
-if user_input.lower() == "yes":
-    deposit_amount = float(input("How much would you like to deposit?"))
-    # Add the deposit_amount to the account balance
-    bank.balance += deposit_amount
-    print("Deposit successful")
-else:
-    print("No deposit will be made.")
-deposit = input("How much would you like to deposit") 
-user_input = input("Do you want to deposit? (yes/no)")
-if user_input.lower() == "yes":
-    deposit_amount = float(input("How much would you like to deposit?"))
-    # Add the deposit_amount to the account balance
-    self.balance += deposit_amount
-    print("Deposit successful")
-else:
-    print("No deposit will be made.")
+bank = BankSystem()
+bank.deposit()
+bank.withdraw()
+                  
+     
